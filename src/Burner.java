dbbe595e14346a93af36bdb3c9a959c0f2dfc8c9
alexvoneida <alexvoneida@gmail.com>
2026-02-03 14:10:19 -0700

    burner methods

--- src/Burner.java
+++ src/Burner.java
@@ -10,4 +10,57 @@ public class Burner {
 	Setting mySetting;
 	int timer;
 	public final static int TIME_DURATION = 2;
+
+	public Temperature getTemperature() {
+		return temperature;
+	}
+
+	public Burner() {
+		super();
+		this.temperature = Temperature.COLD;
+		this.mySetting = Setting.OFF;
+		this.timer = 0;
+	}
+
+	public void plusButton() {
+		timer = TIME_DURATION;
+		switch (mySetting) {
+			case Setting.OFF:
+				mySetting = Setting.LOW;
+				break;
+			case Setting.LOW:
+				mySetting = Setting.MEDIUM;
+				break;
+			case Setting.MEDIUM:
+				mySetting = Setting.HIGH;
+				break;
+			case Setting.HIGH:
+				break;
+		}
+	}
+	
+	public void minusButton() {
+		timer = TIME_DURATION;
+		switch (mySetting) {
+		case Setting.OFF:
+			break;
+		case Setting.LOW:
+			mySetting = Setting.OFF;
+			break;
+		case Setting.MEDIUM:
+			mySetting = Setting.LOW;
+			break;
+		case Setting.HIGH:
+			mySetting = Setting.MEDIUM;
+			break;
+		}
+	}
+	
+	public void updateTemperature() {
+		if (timer > 0) {
+			timer -= 1;
+			
+		}
+	}
+
 }
